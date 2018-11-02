a7e258af7a7db3842a73a4c233bcb7dd188fcbd1
Robert Gore <DarkburstGX@gmail.com>
2018-11-02 14:40:57 -0700

    Second Commit of lesson assignment 2.3.  It vastly expands on the online marketplace assignment in index2.  Troubleshooting needed prior to finalization.

---
+++ fidgetech-daily-planning-110218.md
@@ -0,0 +1,37 @@
+# Daily Planning Doc
+
+##### Date: 11/02/2018
+
+## BEFORE BEGINNING WORK:
+
+
+#### Please list the names of the lessons you are going to work on today:
+
+* Styling and finalization for online marketplace assignment.
+
+* Start Address Book assignment.
+
+
+
+#### Please list 3 concepts you are going to focus on today. (For example, css grids, html forms, string methods, etc.):
+
+1. CSS styles
+
+2. JavaScript functions
+
+3. HTML forms
+
+
+
+## BEFORE YOU LEAVE:
+
+
+#### Please list the names of the lessons you completed today:
+
+* Near-finalization of online marketplace assignment.  Troubleshooting required for clearing receipt for multiple submits.  Assignment: 2.3 Form Input Types: Online Marketplace
+
+
+
+#### Please list the names of the projects you worked on today:
+
+* 2.3 Form Input Types: Online Marketplace (status: Almost complete.  Troubleshooting needed.)
--- js/scripts2.js
+++ js/scripts2.js
@@ -1,3 +1,13 @@
+var burgerCostA = 2.99;
+var burgerCostB = 3.99;
+var burgerCostC = 5.99;
+var friesCostA = 0.99;
+var friesCostB = 1.49;
+var friesCostC = 1.99;
+var drinkCostA = 1.49;
+var drinkCostB = 2.49;
+var drinkCostC = 1.99;
+
 var	totalCostBurgerA = function(burgerCostA, burgerCountA){
   return burgerCostA * burgerCountA;
 }
@@ -14,8 +24,40 @@ var totalCostBurgersAll = function(totalCostBurgerA, totalCostBurgerB, totalCost
   return totalCostBurgerA + totalCostBurgerB + totalCostBurgerC;
 }
 
-function burgerAReceipt(burgerCountA) {
-  if (burgerCountA > 0) {
+var	totalCostFriesA = function(friesCostA, friesCountA){
+  return friesCostA * friesCountA;
+}
+
+var	totalCostFriesB = function(friesCostB, friesCountB){
+  return friesCostB * friesCountB;
+}
+
+var	totalCostFriesC = function(friesCostC, friesCountC){
+  return friesCostC * friesCountC;
+}
+
+var totalCostFriesAll = function(totalCostFriesA, totalCostFriesB, totalCostFriesC) {
+  return totalCostFriesA + totalCostFriesB + totalCostFriesC;
+}
+
+var	totalCostDrinksA = function(drinkCostA, drinksCountA){
+  return drinkCostA * drinksCountA;
+}
+
+var	totalCostDrinksB = function(drinkCostB, drinksCountB){
+  return drinkCostB * drinksCountB;
+}
+
+var	totalCostDrinksC = function(drinkCostC, drinksCountC){
+  return drinkCostC * drinksCountC;
+}
+
+var totalCostDrinksAll = function(totalCostDrinksA, totalCostDrinksB, totalCostDrinksC) {
+  return totalCostDrinksA + totalCostDrinksB + totalCostDrinksC;
+}
+
+function burgerReceiptCheck(burgerCountA, burgerCountB, burgerCountC) {
+  if (burgerCountA > 0 || burgerCountB > 0 || burgerCountC > 0) {
     return true;
   }
   else {
@@ -23,8 +65,8 @@ function burgerAReceipt(burgerCountA) {
   }
 }
 
-function burgerBReceipt(burgerCountB) {
-  if (burgerCountB > 0) {
+function friesReceiptCheck(friesCountA, friesCountB, friesCountC) {
+  if (friesCountA > 0 || friesCountB > 0 || friesCountC > 0) {
     return true;
   }
   else {
@@ -32,8 +74,8 @@ function burgerBReceipt(burgerCountB) {
   }
 }
 
-function burgerCReceipt(burgerCountC) {
-  if (burgerCountC > 0) {
+function drinksReceiptCheck(drinksCountA, drinksCountB, drinksCountC) {
+  if (drinksCountA > 0 || drinksCountB > 0 || drinksCountC > 0) {
     return true;
   }
   else {
@@ -41,43 +83,80 @@ function burgerCReceipt(burgerCountC) {
   }
 }
 
-// var	totalCostD = function(burgerCost, burgerCount){
-//   return burgerCost * burgerCount;
-// }
-//
-// var	totalCostF = function(burgerCost, burgerCount){
-//   console.log(burgerCost, burgerCount);
-//   return burgerCost * burgerCount;
-// }
-
+var receiptCheckAll = function (burgerReceiptCheck, friesReceiptCheck, drinksReceiptCheck){
+  if (burgerReceiptCheck(burgerCountA, burgerCountB, burgerCountC) || friesReceiptCheck(friesCountA, friesCountB, friesCountC) || drinksReceiptCheck(drinksCountA, drinksCountB, drinksCountC)){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
 
 $(document).ready(function() {
   $("form#testForm").submit(function(event) {
     event.preventDefault();
 
-		var burgerCostA = 2.99;
-    var burgerCostB = 3.99;
-    var burgerCostC = 5.99;
-		var burgerCountA = parseInt($("#testInputA").val());
-    var burgerCountB = parseInt($("#testInputB").val());
-    var burgerCountC = parseInt($("#testInputC").val());
-//    var preciseTotal = parseFloat(totalCost(burgerCost, burgerCount)).toFixed(2);
-    var refinedCostBurger = parseFloat(totalCostBurgersAll(totalCostBurgerA(burgerCostA, burgerCountA), totalCostBurgerB(burgerCostB, burgerCountB), totalCostBurgerC(burgerCostC, burgerCountC))).toFixed(2);
+    $("ul#receiptOutput").empty();
+    $("ul#foodOutput").empty();
+    $("ul#receiptTotal").empty();
 
-    if (burgerAReceipt(burgerCountA) || burgerBReceipt(burgerCountB) || burgerCReceipt(burgerCountC)) {
+		var burgerCountA = parseInt($("#burgerPlainInput").val());
+    var burgerCountB = parseInt($("#burgerDoubleInput").val());
+    var burgerCountC = parseInt($("#burgerDeluxeInput").val());
+
+		var friesCountA = parseInt($("#smlFryInput").val());
+    var friesCountB = parseInt($("#medFryInput").val());
+    var friesCountC = parseInt($("#lrgFryInput").val());
+
+		var drinksCountA = parseInt($("#softDrinkInput").val());
+    var drinksCountB = parseInt($("#milkshakeInput").val());
+    var drinksCountC = parseInt($("#juiceDrinksInput").val());
+
+    var refinedCostBurger = parseFloat(totalCostBurgersAll(totalCostBurgerA(burgerCostA, burgerCountA), totalCostBurgerB(burgerCostB, burgerCountB), totalCostBurgerC(burgerCostC, burgerCountC))).toFixed(2);
+    var refinedCostFries = parseFloat(totalCostFriesAll(totalCostFriesA(friesCostA, friesCountA), totalCostFriesB(friesCostB, friesCountB), totalCostFriesC(friesCostC, friesCountC))).toFixed(2);
+    var refinedCostDrinks = parseFloat(totalCostDrinksAll(totalCostDrinksA(drinkCostA, drinksCountA), totalCostDrinksB(drinkCostB, drinksCountB), totalCostDrinksC(drinkCostC, drinksCountC))).toFixed(2);
+    var subtotal = parseFloat(refinedCostBurger) + parseFloat(refinedCostFries) + parseFloat(refinedCostDrinks);
+    var refinedCostTotal = parseFloat(subtotal).toFixed(2);
+
+    if (receiptCheckAll) {
       $("div#receiptOutput").before("<p>An Order has been placed at the restaurant successfully.  Here is your receipt:</p>");
+      $("div#receiptTotal").prepend("<p>$" + refinedCostTotal);
     }
-    if (burgerAReceipt(burgerCountA)) {
-      $("ul#testOutput").append("<li>" + burgerCountA + " Cheeseburger(s): </li>");
+    if (burgerCountA > 0) {
+      $("ul#foodOutput").append("<li>" + burgerCountA + " Cheeseburger(s): </li>");
       $("div#costOutput").append("<p class='money'>$ " + totalCostBurgerA(burgerCostA, burgerCountA));
     }
-    if (burgerBReceipt(burgerCountB)) {
-      $("ul#testOutput").append("<li>" + burgerCountB + " Double Cheeseburger(s): </li>");
+    if (burgerCountB > 0) {
+      $("ul#foodOutput").append("<li>" + burgerCountB + " Double Cheeseburger(s): </li>");
       $("div#costOutput").append("<p class='money'>$ " + totalCostBurgerB(burgerCostB, burgerCountB));
     }
-    if (burgerCReceipt(burgerCountC)) {
-      $("ul#testOutput").append("<li>" + burgerCountC + " Deluxe Cheeseburger(s): </li>");
+    if (burgerCountC > 0) {
+      $("ul#foodOutput").append("<li>" + burgerCountC + " Deluxe Cheeseburger(s): </li>");
       $("div#costOutput").append("<p class='money'>$ " + totalCostBurgerC(burgerCostC, burgerCountC));
+    }
+    if (friesCountA > 0) {
+      $("ul#foodOutput").append("<li>" + friesCountA + " Small Fries: </li>");
+      $("div#costOutput").append("<p class='money'>$ " + totalCostFriesA(friesCostA, friesCountA));
+    }
+    if (friesCountB > 0) {
+      $("ul#foodOutput").append("<li>" + friesCountB + " Medium Fries: </li>");
+      $("div#costOutput").append("<p class='money'>$ " + totalCostFriesB(friesCostB, friesCountB));
+    }
+    if (friesCountC > 0) {
+      $("ul#foodOutput").append("<li>" + friesCountC + " Large Fries: </li>");
+      $("div#costOutput").append("<p class='money'>$ " + totalCostFriesC(friesCostC, friesCountC));
+    }
+    if (drinksCountA > 0) {
+      $("ul#foodOutput").append("<li>" + drinksCountA + " Soft Drink(s): </li>");
+      $("div#costOutput").append("<p class='money'>$ " + totalCostDrinksA(drinkCostA, drinksCountA));
+    }
+    if (drinksCountB > 0) {
+      $("ul#foodOutput").append("<li>" + drinksCountB + " Milkshake(s): </li>");
+      $("div#costOutput").append("<p class='money'>$ " + totalCostDrinksB(drinkCostB, drinksCountB));
+    }
+    if (drinksCountC > 0) {
+      $("ul#foodOutput").append("<li>" + drinksCountC + " Juice Drinks(s): </li>");
+      $("div#costOutput").append("<p class='money'>$ " + totalCostDrinksC(drinkCostC, drinksCountC));
     }
 	});
 });
